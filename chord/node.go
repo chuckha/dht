ce5649a96d0e915421a1836ff84eceb7a6666cdb
Chuck Ha <ha.chuck@gmail.com>
2013-04-10 12:10:27 -0400

    REFACTOR!

--- chord/node.go
+++ chord/node.go
@@ -1,130 +1,38 @@
 package chord
 
 import (
-	"net/http"
-	"net/rpc"
-	"hash/fnv"
-	"fmt"
-	"net"
+	"crypto/sha1"
+	"math/big"
 )
 
-const (
-	m    = uint64(64)
-	max  = ^uint64(1) // ring is from 0 to 2^m -1
-	null = ^uint64(0) // use max as a nil value as it is outside the valid range
-)
-
-type Data struct {
-	K uint64
-	V interface{}
-}
+// Node
 
 type Node struct {
-	Addr        string
-	Id          uint64
-	Data        map[uint64]interface{}
-	Fingers     []*Node
-	Predecessor *Node
+	// Strings are "address:port"
+	fingers []string
+	predecessor string
 }
 
-func NewNode(addr string) *Node {
-	hasher := fnv.New64a()
-	hasher.Write([]byte(addr))
-	// Number won't be bigger than max
-	id := hasher.Sum64()
-	node := &Node{
-		Addr:        addr,
-		Id:          id,
-		Data:        make(map[uint64]interface{}),
-		Fingers:     make([]*Node, 1),
-		Predecessor: new(Node),
+func (n *Node) findSuccessor(*big.Int) {}
+func (n *Node) create() {}
+func (n *Node) join(*big.Int) {}
+func (n *Node) stabilize() {}
+func (n *Node) notify(*big.Int) {}
+func (n *Node) fixFingers() {}
+func (n *Node) checkPredecessor() {}
+
+// Helpers
+
+func hashString(val string) *big.Int {
+	hasher := sha1.New()
+	hasher.Write([]byte(val))
+	return new(big.Int).SetBytes(hasher.Sum(nil))
+}
+
+// Upper end inclusive (start, end]
+func between(val, start, end *big.Int) bool {
+	if val.Cmp(start) <= 0 || val.Cmp(end) > 0 {
+		return false
 	}
-	// A new node should look for itself as a successor until otherwise noted
-	node.Predecessor.Id = null
-	node.Fingers[0] = node
-	go node.listenRPC()
-	return node
+	return true
 }
-
-func (n *Node) listenRPC() {
-	rpc.Register(n)
-	rpc.HandleHTTP()
-	l, e := net.Listen("tcp", fmt.Sprintf(":%v", n.Addr))
-	if e != nil {
-		panic(e)
-	}
-	//fmt.Printf("Now listening on port %v \n", &n.Port)
-	http.Serve(l, nil)
-}
-
-func (n *Node) DialRPC(port *string, reply *rpc.Client) error {
-	reply, err := rpc.DialHTTP("tcp", fmt.Sprintf(":%v", *port))
-	return err
-}
-
-func (n *Node) Get(d *Data, reply *interface{}) error {
-	var succ *Node
-	n.FindSuccessor(&d.K, succ)
-	value := succ.Data[d.K]
-	reply = &value
-	return nil
-}
-
-func (n *Node) Set(d *Data, reply *bool) error {
-	var succ *Node
-	n.FindSuccessor(&d.K, succ)
-	succ.Data[d.K] = d.V
-	*reply = true
-	return nil
-}
-
-// Naive finder
-func (n *Node) FindSuccessor(id *uint64, reply *Node) error {
-	// Uh, this shouldn't recurse forever when there is only one node
-	if n.Id == *id {
-		reply = n
-	} else if Between(*id, n.Id, (n.Fingers[0].Id+1)%max) {
-		reply = n.Fingers[0]
-	} else {
-		// RPC recursion
-		n.Fingers[0].FindSuccessor(id, reply)
-	}
-	return nil
-}
-
-// Setter to Join a ring
-func (n *Node) Join(m *Node, reply *bool) error {
-	n.Fingers[0] = m
-	*reply = true
-	return nil
-}
-
-// Call this every so often
-func (n *Node) stabilize() {
-	x := n.Fingers[0].Predecessor
-	if x.Id != null && Between(x.Id, n.Id, n.Fingers[0].Id) {
-		n.Fingers[0] = x
-	}
-	n.Fingers[0].notify(n)
-}
-
-func (n *Node) notify(m *Node) {
-	if n.Predecessor.Id == null || Between(m.Id, n.Predecessor.Id, n.Id) {
-		n.Predecessor = m
-	}
-	if n.Fingers[0] == n {
-		n.Fingers[0] = m
-	}
-}
-
-// l < x <= r
-func Between(x, l, r uint64) bool {
-	if l > r {
-		return x > l && x <= max || x >= 0 && x < r
-	}
-	return x > l && x <= r
-}
-
-
-//func (n *node) closestpreceedingnode(id int) {
-//}
--- chord/node_test.go
+++ chord/node_test.go
@@ -1,157 +1,32 @@
 package chord
 
 import (
+	"math/big"
 	"testing"
 )
 
-func TestBetweenPassCases(t *testing.T) {
-	vals := [][]uint64{
-		{9, 0, 10},
-		{21, 15, 50},
-		{max, max - 4, 3},
-		{0, max, 2},
-		{max, max - 1, 0},
-		{max - 1, max - 2, 0},
+func TestBetween(t *testing.T) {
+	testTrueCases := [][]*big.Int{
+		{big.NewInt(4), big.NewInt(2), big.NewInt(5)},
+		{big.NewInt(5), big.NewInt(2), big.NewInt(5)},
+		{big.NewInt(-10), big.NewInt(-13), big.NewInt(-8)},
 	}
-	for _, val := range vals {
-		v := Between(val[0], val[1], val[2])
-		if !v {
-			t.Errorf("%d is between %d and %d", val[0], val[1], val[2])
+	testFalseCases := [][]*big.Int{
+		{big.NewInt(2), big.NewInt(2), big.NewInt(5)},
+		{big.NewInt(0), big.NewInt(2), big.NewInt(5)},
+		{big.NewInt(6), big.NewInt(2), big.NewInt(5)},
+		{big.NewInt(-1), big.NewInt(85), big.NewInt(232323)},
+	}
+	for _, testCase := range testTrueCases {
+		result := between(testCase[0], testCase[1], testCase[2])
+		if !result {
+			t.Errorf("This test case should be true: %v in (%v, %v]", testCase[0], testCase[1], testCase[2])
 		}
 	}
-}
-
-func TestBetweenFailCases(t *testing.T) {
-	vals := [][]uint64{
-		{0, 0, 10},
-		{51, 15, 50},
-		{max - 4, max - 4, 3},
-		{100, max, 2},
-		{max - 5, max - 1, 0},
-		{1000000, max - 1, 0},
-	}
-	for _, val := range vals {
-		v := Between(val[0], val[1], val[2])
-		if v {
-			t.Errorf("%d is not between %d and %d", val[0], val[1], val[2])
+	for _, testCase := range testFalseCases {
+		result := between(testCase[0], testCase[1], testCase[2])
+		if result {
+			t.Errorf("This test case should be false: %v in (%v, %v]", testCase[0], testCase[1], testCase[2])
 		}
-	}
-}
-
-func TestOneNode(t *testing.T) {
-	n := NewNode("10000")
-	t.Log(n.Id)
-	if n.Id > max {
-		t.Errorf("This can't be > 1024!")
-	}
-}
-
-func TestFindSuccessorOfOneNode(t *testing.T) {
-	n := NewNode("10101")
-	num := uint64(100)
-	node := n.findSuccessor(&num)
-	if node != n {
-		t.Errorf("The one node is its own successor")
-	}
-}
-
-func TestFindSuccessorOfTwoNodes(t *testing.T) {
-	n := NewNode("10101")
-	n2 := NewNode("10000")
-
-	var x *bool
-	n2.Join(n, x)
-	if n2.Fingers[0] != n {
-		t.Errorf("Successor of n2 should definitely be n")
-	}
-}
-
-func TestGetOneNode(t *testing.T) {
-	n1 := NewNode("10000")
-	d := Data{123, "hello world"}
-	n1.Data[d.K] = d.V
-	data := new(interface{})
-	n1.Get(&d, data)
-	if *data != "hello world" {
-		t.Errorf("Data should be the same")
-	}
-}
-
-func TestSetTwoNodes(t *testing.T) {
-	n1 := NewNode("10000")
-	n2 := NewNode("10002")
-	var x *bool
-	n2.Join(n1, x)
-	n2.stabilize()
-	n1.stabilize()
-	n2.stabilize()
-	n1.stabilize()
-	t.Logf("n1: %v", n1.Id)
-	t.Logf("n2: %v", n2.Id)
-	t.Logf("n1 Succ: %v", n1.Fingers[0].Id)
-	t.Logf("n2 Succ: %v", n2.Fingers[0].Id)
-	t.Logf("n1 pred: %v", n1.Predecessor.Id)
-	t.Logf("n2 pred: %v", n2.Predecessor.Id)
-
-	data1 := Data{123, "hello world"}
-	data2 := Data{9431690416212140030, "Go on"}
-	var b1, b2 *bool
-
-	n1.Set(&data1, b1)
-	n1.Set(&data2, b2)
-
-	if n1.Data[data1.K] != "hello world" {
-		t.Errorf("n1 should be responsible for this key")
-	}
-	if n1.Data[data2.K] == "Go on" {
-		t.Errorf("n1 Should not have this key")
-	}
-	if n2.Data[data2.K] != "Go on" {
-		t.Errorf("n2 should be responsible for this key")
-	}
-
-
-}
-
-
-func TestStabilize(t *testing.T) {
-	n1 := NewNode("10000")
-	n2 := NewNode("10001")
-	n3 := NewNode("10002")
-	n4 := NewNode("10003")
-	t.Logf("n1 ID: %v", n1.Id)
-	t.Logf("n2 ID: %v", n2.Id)
-	t.Logf("n3 ID: %v", n3.Id)
-	t.Logf("n4 ID: %v", n4.Id)
-	var x *bool
-	n2.Join(n1, x)
-	n3.Join(n1, x)
-	n4.Join(n1, x)
-	t.Logf("Joined and Stabilizing")
-	for i := 0; i < 5; i ++ {
-		n4.stabilize()
-		n3.stabilize()
-		n2.stabilize()
-		n1.stabilize()
-	}
-	t.Logf("n1 Predecessor: %v", n1.Predecessor.Id)
-	t.Logf("n1 Successor: %v", n1.Fingers[0].Id)
-	t.Logf("n2 Predecessor: %v", n2.Predecessor.Id)
-	t.Logf("n2 Successor: %v", n2.Fingers[0].Id)
-	t.Logf("n3 Predecessor: %v", n3.Predecessor.Id)
-	t.Logf("n3 Successor: %v", n3.Fingers[0].Id)
-	t.Logf("n4 Predecessor: %v", n4.Predecessor.Id)
-	t.Logf("n4 Successor: %v", n4.Fingers[0].Id)
-	if n1.Fingers[0] != n2 {
-		t.Errorf("n1 successor should be n2!")
-	}
-	if n2.Fingers[0] != n3 {
-		t.Errorf("n2 successor should be n3!")
-	}
-	if n3.Fingers[0] != n4 {
-		t.Errorf("n3 successor should be n4!")
-	}
-	if n4.Fingers[0] != n1 {
-		t.Errorf("n4 successor should be n1!")
 	}
 }
